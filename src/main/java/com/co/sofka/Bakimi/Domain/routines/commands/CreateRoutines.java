ce5041129da7669c73dfe00cb059a02f9cdfae9e
saracarohdm <saracarohdm@gmail.com>
2021-06-21 12:37:21 -0500

    Se modifico atributos de el evento Createroutines y se creo el controller

--- src/main/java/com/co/sofka/Bakimi/Domain/routines/commands/CreateRoutines.java
+++ src/main/java/com/co/sofka/Bakimi/Domain/routines/commands/CreateRoutines.java
@@ -7,17 +7,13 @@ import com.co.sofka.Bakimi.Domain.routines.values.*;
 public class CreateRoutines implements Command {
     private  final RoutinesId routinesId;
     private final RoutinesName routinesName;
-    private final Products products;
     private final IdUsuario idUsuario;
-    private final DescriptionRoutines descriptionRoutines;
     private final TypeSkin typeSkin;
 
-    public CreateRoutines(RoutinesId routinesId, RoutinesName routinesName, Products products, IdUsuario idUsuario, DescriptionRoutines descriptionRoutines, TypeSkin typeSkin) {
+    public CreateRoutines(RoutinesId routinesId, RoutinesName routinesName, IdUsuario idUsuario, TypeSkin typeSkin) {
         this.routinesId = routinesId;
         this.routinesName = routinesName;
-        this.products = products;
         this.idUsuario = idUsuario;
-        this.descriptionRoutines = descriptionRoutines;
         this.typeSkin = typeSkin;
     }
 
@@ -29,16 +25,8 @@ public class CreateRoutines implements Command {
         return routinesName;
     }
 
-    public Products Products() {
-        return products;
-    }
-
     public IdUsuario IdUsuario() {
         return idUsuario;
-    }
-
-    public DescriptionRoutines DescriptionRoutines() {
-        return descriptionRoutines;
     }
 
     public TypeSkin TypeSkin() {
