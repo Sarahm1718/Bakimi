6e8fb371b14e752c0dece17fb8afe53385832dae
saracarohdm <saracarohdm@gmail.com>
2021-06-21 12:12:23 -0500

    Se modifico el caso de uso de routines

--- src/main/java/com/co/sofka/Bakimi/Domain/routines/useCase/CreateRoutinesUseCase.java
+++ src/main/java/com/co/sofka/Bakimi/Domain/routines/useCase/CreateRoutinesUseCase.java
@@ -2,16 +2,39 @@ package com.co.sofka.Bakimi.Domain.routines.useCase;
 
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
-import co.com.sofka.business.support.ResponseEvents;
 import com.co.sofka.Bakimi.Domain.routines.Routines;
 import com.co.sofka.Bakimi.Domain.routines.commands.CreateRoutines;
+import com.co.sofka.Bakimi.Domain.routines.repository.IRoutineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class CreateRoutinesUseCase extends UseCase<RequestCommand<CreateRoutines>, ResponseEvents> {
+public class CreateRoutinesUseCase extends UseCase<RequestCommand<CreateRoutines>, CreateRoutinesUseCase.Response> {
+
+    @Autowired
+    private IRoutineRepository iRoutineRepository;
 
     @Override
     public void executeUseCase(RequestCommand<CreateRoutines> createRoutinesRequestCommand) {
             var command = createRoutinesRequestCommand.getCommand();
             var routines = new Routines(command.RoutinesId(), command.RoutinesName(), command.Products(), command.DescriptionRoutines(), command.IdUsuario(), command.TypeSkin());
-            emit().onResponse(new ResponseEvents(routines.getUncommittedChanges()));
+            iRoutineRepository.save(routines);
+            emit().onResponse(new Response(routines));
         }
+
+    public static class Response implements UseCase.ResponseValues{
+
+        private Routines response;
+
+
+        public Response(Routines routines) {
+            this.response=routines;
+        }
+
+        public Routines getResponse() {
+            return response;
+        }
+
+        public void setResponse(Routines response) {
+            this.response = response;
+        }
+    }
     }
