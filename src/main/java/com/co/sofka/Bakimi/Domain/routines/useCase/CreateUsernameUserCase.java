5161852c7d95adf011c4128539699aec0b698933
saracarohdm <saracarohdm@gmail.com>
2021-06-24 17:26:44 -0500

    se modifico el caso de uso de username

--- src/main/java/com/co/sofka/Bakimi/Domain/routines/useCase/CreateUsernameUserCase.java
+++ src/main/java/com/co/sofka/Bakimi/Domain/routines/useCase/CreateUsernameUserCase.java
@@ -3,24 +3,43 @@ package com.co.sofka.Bakimi.Domain.routines.useCase;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
-import co.com.sofka.domain.generic.DomainEvent;
-import com.co.sofka.Bakimi.Domain.routines.commands.CreateUsername;
 import com.co.sofka.Bakimi.Domain.routines.Username;
-import com.co.sofka.Bakimi.Domain.routines.events.UsernameCreated;
+import com.co.sofka.Bakimi.Domain.routines.commands.CreateUsername;
+import com.co.sofka.Bakimi.Domain.routines.repository.IRoutineRepository;
+import com.co.sofka.Bakimi.Domain.routines.repository.IUsernameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+@Service
+public class CreateUsernameUserCase  extends UseCase<RequestCommand<CreateUsername>, CreateUsernameUserCase.Response> {
 
-public class CreateUsernameUserCase  extends UseCase<RequestCommand<CreateUsername>, ResponseEvents> {
+    @Autowired
+    private IUsernameRepository iUsernameRepository;
 
     @Override
     public void executeUseCase(RequestCommand<CreateUsername> createUsernameRequestCommand) {
         var command = createUsernameRequestCommand.getCommand();
-        new Username(command.IdUsuario(), command.Name(), command.Email(), command.TypeSkin());
-        List<DomainEvent> ListEvents;
-        ListEvents=new ArrayList<>();
-        ListEvents.add(new UsernameCreated(command.Name(), command.Email(), command.TypeSkin(), command.IdUsuario()));
-        emit().onResponse(new ResponseEvents(ListEvents));
+        var username = new Username(command.IdUsuario(), command.Name(), command.Email(), command.TypeSkin());
+        iUsernameRepository.save(username);
+        emit().onResponse(new Response(username));
 
     }
+    public static class Response implements UseCase.ResponseValues{
+
+        private Username response;
+
+
+        public Response(Username username) {
+            this.response=username;
+        }
+
+        public Username getResponse() {
+            return response;
+        }
+
+        public void setResponse(Username response) {
+            this.response = response;
+        }
+    }
 }
+
